39c09ed5a331b60ca997b153b532f5d228261c1f
Vitor <vhpsantos@gmail.com>
2022-12-10 16:00:32 -0300

    Upando arquivos para GitHub

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '162.214.122.42'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'fazag_web'),
+      user: env('DATABASE_USERNAME', 'fazag_admin'),
+      password: env('DATABASE_PASSWORD', 'zszsxu5973'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
